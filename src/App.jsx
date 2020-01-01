62b96b1ce9d74ddaf1b1c338bd76f94874e6915c
kabeh2 <mattcnk@hotmail.com>
2020-01-01 17:23:21 -0500

    Updated readme and gh-pages setup

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useReducer } from "react";
+
+import { initialState, reducer } from "./reducers";
+import "./App.scss";
+import Tabs from "./components/Tabs";
+import List from "./components/List";
+import TodoForm from "./components/TodoForm";
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const handleChange = e => {
+    dispatch({ type: "INPUT", value: e.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    dispatch({
+      type: "SUBMIT",
+      payload: {
+        task: state.todo,
+        id: new Date(),
+        completed: false
+      }
+    });
+  };
+
+  const handleChecked = (index, todo) => {
+    dispatch({
+      type: "COMPLETED",
+      index,
+      payload: [
+        {
+          task: todo.task,
+          id: todo.id,
+          completed: !todo.completed
+        }
+      ]
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>React ToDo List w/ useReducer</h1>
+      <TodoForm
+        {...state}
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
+      <Tabs {...state} dispatch={dispatch} />
+      <List {...state} handleChecked={handleChecked} />
+    </div>
+  );
+}
+
+export default App;
