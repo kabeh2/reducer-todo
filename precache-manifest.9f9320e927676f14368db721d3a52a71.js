f7b4b9d7334e7022dba4816c9b08bb23944267f8
kabeh2 <mattcnk@hotmail.com>
2020-01-01 17:22:20 -0500

    Updates

---
+++ precache-manifest.9f9320e927676f14368db721d3a52a71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e12f027d4b367457f8d621bd2bd6573",
+    "url": "/reducer-todo/index.html"
+  },
+  {
+    "revision": "54c3a9d2714871133cc7",
+    "url": "/reducer-todo/static/css/main.a6a37203.chunk.css"
+  },
+  {
+    "revision": "f6d85b8dfe461244b6a5",
+    "url": "/reducer-todo/static/js/2.d20f7344.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/reducer-todo/static/js/2.d20f7344.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54c3a9d2714871133cc7",
+    "url": "/reducer-todo/static/js/main.bf9b7e93.chunk.js"
+  },
+  {
+    "revision": "16b535de32ee429722e0",
+    "url": "/reducer-todo/static/js/runtime-main.6915b046.js"
+  }
+]);
